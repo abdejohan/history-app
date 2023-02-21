5759ca01bb3276ada533ce45ffff34081d858da1
abdejohan <abdejohan@gmail.com>
2023-02-21 16:53:05 +0100

    Display events details if a year is selected

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,36 @@ function App() {
 	const [selectedYear, setSelectedYear] = useState<number>();
 
 	return (
-		<div className='entry'>
-			<div className='line'>
-				<div className='center' />
-				{arrayOfStories.map((story) => (
-					<h6
-						onClick={() => setSelectedYear(story.year)}
-						className={`year ${selectedYear === story.year ? "selected" : ""}`}>
-						{story.year}
-					</h6>
-				))}
+		<main>
+			<div className='years_container'>
+				<h2 className='header'>SELECT A YEAR</h2>
+				<div className='list_of_years'>
+					{arrayOfStories.map((story) => (
+						<h2
+							onClick={() => setSelectedYear(story.year)}
+							className={`year ${selectedYear === story.year ? "selected" : ""}`}>
+							{story.year}
+						</h2>
+					))}
+				</div>
 			</div>
-		</div>
+			<div className='stories_container'>
+				<h2 className='header'>EVENTS THAT YEAR</h2>
+				<div className='list_of_stories'>
+					{arrayOfStories.map((story) => {
+						if (selectedYear === story.year) {
+							return (
+								<div className='story'>
+									<h3>{story.title}</h3>
+									<img src={story.image} alt='image of event' height='300' width='300' />
+									<p>{story.text}</p>
+								</div>
+							);
+						}
+					})}
+				</div>
+			</div>
+		</main>
 	);
 }
 
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -4,12 +4,16 @@ const arrayOfStories = [
 		year: -400,
 		title: "Persion Empire is at its hight",
 		text: "Darius conqueres all of greece",
+		image:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/David_-_Napoleon_crossing_the_Alps_-_Malmaison2.jpg/800px-David_-_Napoleon_crossing_the_Alps_-_Malmaison2.jpg",
 	},
 	{
 		id: 2,
 		year: -300,
 		title: "Alexander The Great is Born",
 		text: "King Philip II has his first son",
+		image:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/David_-_Napoleon_crossing_the_Alps_-_Malmaison2.jpg/800px-David_-_Napoleon_crossing_the_Alps_-_Malmaison2.jpg",
 	},
 ];
 
