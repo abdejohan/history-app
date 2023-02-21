4b9dd61fa84df64cfbdb26b34877d087790f7c3c
abdejohan <abdejohan@gmail.com>
2023-02-21 16:22:20 +0100

    Create list of years that can be selected

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,20 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
-
-const renderPoints = () => {
-	const date = new Date();
-	const currentYear = date.getFullYear();
-	const startingYear = -100;
-	const arrayOfPoints = [];
-
-	for (let i = startingYear; i < 200; i++) {
-		// Remove year 0
-		if (i !== 0) {
-			arrayOfPoints.push(
-				<div className={`point_${i}`} key={i}>
-					<h6>{i}</h6>
-					<div className='hline' />
-				</div>
-			);
-		}
-	}
-	return arrayOfPoints;
-};
+import { useState } from "react";
+import { arrayOfStories } from "./utils";
 
 function App() {
-	const centerRef = useRef<HTMLDivElement>(null);
-	const [centerPosition, setCenterPosition] = useState([0, 0]);
-
-	useEffect(() => {
-		const { x, y } = centerRef.current!.getBoundingClientRect();
-		console.log(x, y);
-
-		const arr = document.elementFromPoint(x, y);
-		console.log(arr);
-	}, []);
+	const [selectedYear, setSelectedYear] = useState<number>();
 
 	return (
 		<div className='entry'>
 			<div className='line'>
-				<div className='center' ref={centerRef} />
-				{renderPoints()}
+				<div className='center' />
+				{arrayOfStories.map((story) => (
+					<h6
+						onClick={() => setSelectedYear(story.year)}
+						className={`year ${selectedYear === story.year ? "selected" : ""}`}>
+						{story.year}
+					</h6>
+				))}
 			</div>
 		</div>
 	);
---
+++ src/utils/index.tsx
@@ -0,0 +1,16 @@
+const arrayOfStories = [
+	{
+		id: 1,
+		year: -400,
+		title: "Persion Empire is at its hight",
+		text: "Darius conqueres all of greece",
+	},
+	{
+		id: 2,
+		year: -300,
+		title: "Alexander The Great is Born",
+		text: "King Philip II has his first son",
+	},
+];
+
+export { arrayOfStories };
