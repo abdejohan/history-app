29b63ff236cd01b5aa37136ec4c256e544f2d682
abdejohan <abdejohan@gmail.com>
2023-02-25 14:47:17 +0100

    Convert chakra style into css for home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,31 +4,28 @@ import YearSelection from "../components/YearSelection";
 
 function Home() {
 	return (
-		<Flex direction='row' grow='1' h='100vh' w='100vw' p='20'>
-			<Flex direction='column'>
-				<Heading as='h1' size='lg' mb='4'>
-					Written History Timeline
-				</Heading>
-				<Heading as='h1' size='sm'>
-					History is fun! But also confusing...
-				</Heading>
-				<Text mb='4' fontSize='small' maxW='400px'>
-					I love history but sometimes i have a hard time remembering at what point in
-					time something happend.
-				</Text>
-				<Text mb='4' fontSize='small' maxW='400px'>
-					Did Julius Cesar march against Gaule before The birth of Christ? Was Darius The
-					Great King of Persia when the battle of marathon took place? And What year did
-					Genghis Khan sack Baghdad?
-				</Text>
-				<Text mb='4' fontSize='small' maxW='400px'>
-					If you just like me, could need a quick reminder of who was where and when, you
-					might find this site useful. Enjoy!
-				</Text>
+		<section>
+			<div className='welcome-info'>
+				<h1>
+					Timeline: <span>Written History </span>
+				</h1>
+				<h2>History is fun! But sometimes confusing...</h2>
+				<div className='welcome-paragraph-wrapper'>
+					<p>I love history but I find myself having a hard time remembering it all.</p>
+					<p>
+						Did Julius Cesar march against Gaule before The birth of Christ? Was Darius
+						The Great King of Persia when the battle of marathon took place? And What year
+						did Genghis Khan sack Baghdad?
+					</p>
+					<p>
+						If you just like me, could need a quick reminder of who was where and when,
+						you might find this site useful. Enjoy!
+					</p>
+				</div>
 				<YearSelection />
-			</Flex>
+			</div>
 			<DisplayEvents />
-		</Flex>
+		</section>
 	);
 }
 
