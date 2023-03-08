d80daa921145aba48fdc81b62bd14ba273e8b3a3
abdejohan <abdejohan@gmail.com>
2023-03-08 18:25:06 +0100

    Change color scheme

--- src/components/WelcomeInfo.tsx
+++ src/components/WelcomeInfo.tsx
@@ -4,21 +4,6 @@ const WelcomeInfo = () => {
 			<h1>
 				Collection of events from <span>ancient history</span>
 			</h1>
-			<h2>
-				Collection of events from <span>ancient history</span>
-			</h2>
-			<h3>
-				Collection of events from <span>ancient history</span>
-			</h3>
-			<h4>
-				Collection of events from <span>ancient history</span>
-			</h4>
-			<h5>
-				Collection of events from <span>ancient history</span>
-			</h5>
-			<h6>
-				Collection of events from <span>ancient history</span>
-			</h6>
 			<p>
 				Did Julius Cesar march against Gaule before the birth of Christ? Who was the king
 				of Persia at the battle of Marathon, and what year did Genghis Khan sack Baghdad?
