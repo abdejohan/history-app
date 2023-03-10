c5b909db0ec68de7a90da67243e743fcf29cc654
abdejohan <abdejohan@gmail.com>
2023-03-10 14:09:25 +0100

    Remove unsued styles

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,23 +1,15 @@
-import DisplayEvents from "../components/DisplayEvents";
 import WelcomeInfo from "../components/WelcomeInfo";
 import SelectCentury from "../components/SelectCentury";
-import Button from "../common/Button";
-import { scrollToElement } from "../utils";
+import ContentView from "../components/ContentView";
 
 function Home() {
 	return (
-		<section className='home-container'>
-			<section className='full-page-container'>
+		<main>
+			<ContentView>
 				<WelcomeInfo />
-				<div>
-					<Button label='Explore' onClick={() => scrollToElement("select-century")} />
-					<Button label='Add Event' onClick={() => console.log("EXPLORE")} />
-				</div>
-			</section>
-			<section className='full-page-container' id='select-century'>
 				<SelectCentury />
-			</section>
-		</section>
+			</ContentView>
+		</main>
 	);
 }
 
