1d3cbadc923d6e353eab5abeeceeb0e7e0dc7518
abdejohan <abdejohan@gmail.com>
2023-03-17 00:39:04 +0100

    Add faded background icon on all pages

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import { GiEgyptianPyramids } from "react-icons/gi";
+import Footer from "../common/Footer";
 
 const Layout = () => (
 	<main className='layout'>
+		<GiEgyptianPyramids className='timeline-corner-icon dd' />
 		<Header />
 		<Outlet />
+		<Footer />
 	</main>
 );
 
