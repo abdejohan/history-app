db961848c1f697bb3b684c04cba5db35f46d05d9
abdejohan <abdejohan@gmail.com>
2023-03-17 12:22:35 +0100

    Add github link and style header navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,20 @@
+import { GiAncientColumns } from "react-icons/gi";
+import { BsGithub } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const Header = () => {
 	return (
 		<header>
 			<nav>
-				<Link to='/'>Home</Link>
-				<Link to='/admin'>Add Event</Link>
-				<Link to='/admin'>Contact</Link>
+				<div style={{ display: "flex" }}>
+					<GiAncientColumns className='header-logo' />
+					<Link to='/'>Home</Link>
+					<Link to='/admin'>Add Event</Link>
+					<Link to='/learn-more'>Learn more</Link>
+				</div>
+				<Link to='https://github.com/abdejohan' target='_blank'>
+					<BsGithub className='github' color='white' />
+				</Link>
 			</nav>
 		</header>
 	);
