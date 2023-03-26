3ea51bb5ec0a8511c91294e09bbb715048f62b16
abdejohan <abdejohan@gmail.com>
2023-03-26 17:50:38 +0200

    Add 'no results found' message

--- src/components/DisplayEvents.tsx
+++ src/components/DisplayEvents.tsx
@@ -1,7 +1,9 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 import { HistoryEvent } from "../types";
 import ContentView from "./ContentView";
 import TimelineItem from "./TimelineItem";
+import { TfiFaceSad } from "react-icons/tfi";
 
 // historyEvents.map((event) => <TimelineItem event={event} key={event.title} />)
 interface DisplayEventsProps {
@@ -17,6 +19,13 @@ const DisplayEvents: FC<DisplayEventsProps> = ({ events, century }) => {
 				{events &&
 					events.map((event, index) => <TimelineItem event={event} key={index} />)}
 			</ul>
+			{!events.length && (
+				<div className='no-events-found-container'>
+					<TfiFaceSad size={50} />
+					<p>Looks like there is no events for this century..</p>
+					<Link to='/admin'>Add event? </Link>
+				</div>
+			)}
 		</ContentView>
 	);
 };
